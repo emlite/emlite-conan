909d531c079f7bbef22ef45a0a443988fc208cff
Mohammed Alyousef <may642_2000@hotmail.com>
2025-08-23 14:14:54 +0300

    add emcore

--- recipes/emcore/all/conanfile.py
+++ recipes/emcore/all/conanfile.py
@@ -7,26 +7,24 @@ import os
 
 required_conan_version = ">=2.0"
 
-class WasmbindConan(ConanFile):
-    name = "wasmbind"
-    version = "0.1.3"
+class EmcoreConan(ConanFile):
+    name = "emcore"
+    version = "0.1.0"
     license = "MIT"
-    url = "https://github.com/emlite/wasmbind"
-    homepage = "https://github.com/emlite/wasmbind"
-    description = "Wasmbind is a C++ project that offers bindings to Web APIs for use with wasm."
-    topics = ("wasm", "emscripten", "webapi", "bindings", "c++")
+    url = "https://github.com/emlite/emcore"
+    homepage = "https://github.com/emlite/emcore"
+    description = "emlite is a tiny JS bridge for native C/C++ code via Wasm."
+    topics = ("wasm", "emscripten", "javascript", "bridge", "c", "c++")
     package_type = "library"
 
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "webbind": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "webbind": False,
     }
 
     def config_options(self):
@@ -35,21 +33,21 @@ class WasmbindConan(ConanFile):
 
     def validate(self):
         if str(self.settings.get_safe("os")) != "Emscripten":
-            raise ConanInvalidConfiguration("wasmbind is intended for os=Emscripten")
+            raise ConanInvalidConfiguration("emlite is intended for os=Emscripten")
 
     def layout(self):
         cmake_layout(self, src_folder=".")
 
     def requirements(self):
-        self.requires("emcore/0.1.0")
-        self.requires("emlite/0.1.3")
+        if str(self.settings.get_safe("os")) == "Emscripten":
+            self.requires("emsenv/0.1.2")
 
     def source(self):
         git = Git(self)
         git.clone(
-            url="https://github.com/emlite/wasmbind.git",
+            "https://github.com/emlite/emcore.git",
             target=".",
-            args=["--depth=1", f"--branch=v{self.version}"],
+            args=["--depth=1", f"--branch=v{self.version}"]
         )
 
     def generate(self):
@@ -60,10 +58,7 @@ class WasmbindConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={
-            "WASMBIND_BUILD_WEBBIND": "ON" if self.options.webbind else "OFF",
-            "WASMBIND_BUILD_EXAMPLES": "OFF",
-        })
+        cmake.configure()
         cmake.build()
 
     def package(self):
@@ -73,6 +68,6 @@ class WasmbindConan(ConanFile):
              dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "wasmbind")
-        self.cpp_info.set_property("cmake_target_name", "wasmbind::wasmbind")
+        self.cpp_info.set_property("cmake_file_name", "emcore")
+        self.cpp_info.set_property("cmake_target_name", "emcore::emcore")
         self.cpp_info.libs = collect_libs(self)
--- recipes/emlite/all/conanfile.py
+++ recipes/emlite/all/conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=2.0"
 
 class EmliteConan(ConanFile):
     name = "emlite"
-    version = "0.1.2"
+    version = "0.1.3"
     license = "MIT"
     url = "https://github.com/emlite/emlite-cpp"
     homepage = "https://github.com/emlite/emlite-cpp"
@@ -21,16 +21,13 @@ class EmliteConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "capi": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "capi": False,
     }
 
     def config_options(self):
-        # fPIC has no effect on Windows
         if self.settings.get_safe("os") == "Windows":
             del self.options.fPIC
 
@@ -39,14 +36,11 @@ class EmliteConan(ConanFile):
             raise ConanInvalidConfiguration("emlite is intended for os=Emscripten")
 
     def layout(self):
-        # If CMakeLists.txt lives at repo root (most common), keep "."
-        # If it's under "src/", change to: cmake_layout(self, src_folder="src")
         cmake_layout(self, src_folder=".")
 
     def requirements(self):
-        # This is your adapter layer for Emscripten specifics
         if str(self.settings.get_safe("os")) == "Emscripten":
-            self.requires("emsenv/0.1.2")
+            self.requires("emcore/0.1.0")
 
     def source(self):
         git = Git(self)
@@ -57,7 +51,6 @@ class EmliteConan(ConanFile):
         )
 
     def generate(self):
-        # Conan 2: produce CMakePresets.json + deps files
         tc = CMakeToolchain(self)
         tc.generate()
         deps = CMakeDeps(self)
@@ -66,7 +59,6 @@ class EmliteConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.configure(variables={
-            "EMLITE_CLIB_ONLY": "ON" if self.options.capi else "OFF",
             "EMLITE_BUILD_EXAMPLES": "OFF",
         })
         cmake.build()
@@ -78,7 +70,6 @@ class EmliteConan(ConanFile):
              dst=os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
-        # CMake integration hints for consumers
         self.cpp_info.set_property("cmake_file_name", "emlite")
         self.cpp_info.set_property("cmake_target_name", "emlite::emlite")
         self.cpp_info.libs = collect_libs(self)
